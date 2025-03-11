d9db9db6fd445af4457eed63c36fb5ae4737fbe2
Rian Guedes <rianguedes.rd@gmail.com>
2025-03-10 23:13:00 -0300

    PostgreSQL com Docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Rian Guedes',
+    email: 'rianguedes.rd@gmail.com',
+  },
+})
 
 export const app = fastify()
