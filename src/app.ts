29720cd5dac9aa926b6e03ae99610d55f6ef8d6f
Rian Guedes <rianguedes.rd@gmail.com>
2025-03-17 22:05:28 -0300

    Add design patterns

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    name: 'Rian Guedes',
-    email: 'rianguedes.rd@gmail.com',
-  },
-})
+import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
+
+app.register(appRoutes)
+
+app.setErrorHandler((error, _, replay) => {
+  if (error instanceof ZodError) {
+    return replay
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return replay.status(500).send({ message: 'Server internal error.' })
+})
