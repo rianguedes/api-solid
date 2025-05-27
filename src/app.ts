d41c8971616135b81e7e38ee782a8f5ba515919b
Rian Guedes <rianguedes.rd@gmail.com>
2025-05-26 21:13:28 -0300

    add docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from './generated/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Rian',
+    email: 'rian@teste.com',
+  },
+})
